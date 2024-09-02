885290a63b24ee831eefdfe5c063c84c63595c97
bxljoy <259500313@qq.com>
2024-09-02 23:43:45 +0200

    feat: update styling of Home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Button, Image } from "react-native";
+import { Button, Image, TouchableOpacity } from "react-native";
 import { Text, View } from "@/components/Themed";
 import * as ImagePicker from "expo-image-picker";
 import { Camera } from "expo-camera";
 import { analyzeImage } from "@/lib/visionApi";
+import * as Clipboard from "expo-clipboard";
+import { MaterialIcons } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabOneScreen() {
   const [hasPermission, setHasPermission] = useState(false);
   const [image, setImage] = useState<string | null>(null);
   const [extractedText, setExtractedText] = useState<string | null>(null);
+  const [isExtracting, setIsExtracting] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -28,8 +32,6 @@ export default function TabOneScreen() {
 
       if (!result.canceled) {
         setImage(result.assets[0].uri);
-        // Here you would call the function to send the image to Google Cloud Vision API
-        // analyzeImage(result.assets[0].uri);
       }
     }
   };
@@ -44,15 +46,26 @@ export default function TabOneScreen() {
 
     if (!result.canceled) {
       setImage(result.assets[0].uri);
-      // Here you would call the function to send the image to Google Cloud Vision API
-      // analyzeImage(result.assets[0].uri);
     }
   };
 
-  const extractContent = async () => {
-    if (image) {
-      const text = await analyzeImage(image);
-      setExtractedText(text);
+  const handleExtractText = async () => {
+    setIsExtracting(true);
+    try {
+      if (image) {
+        const text = await analyzeImage(image);
+        setExtractedText(text);
+      }
+    } catch (error) {
+      console.error("Error extracting text:", error);
+    } finally {
+      setIsExtracting(false);
+    }
+  };
+
+  const copyToClipboard = async () => {
+    if (extractedText) {
+      await Clipboard.setStringAsync(extractedText);
     }
   };
 
@@ -64,20 +77,42 @@ export default function TabOneScreen() {
   }
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold mb-4">
-        OCR with Google Cloud Vision
+    <SafeAreaView className="flex-1 items-center">
+      <Text className="text-2xl font-bold mb-4">InstaLingo</Text>
+      <Text className="text-lg mb-4">
+        Convert your images to your learning material
       </Text>
-      <Button title="Take a picture" onPress={takePicture} />
-      <Button title="Pick an image from gallery" onPress={pickImage} />
-      {image && (
-        <Image
-          source={{ uri: image }}
-          style={{ width: 200, height: 200, marginTop: 20 }}
-        />
+      <View className="flex flex-row justify-around w-full">
+        <TouchableOpacity onPress={takePicture} className="flex items-center">
+          <MaterialIcons name="camera-alt" size={48} color="coral" />
+          <Text className="mt-2">Capture</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={pickImage} className="flex items-center">
+          <MaterialIcons name="photo" size={48} color="coral" />
+          <Text className="mt-2">Album</Text>
+        </TouchableOpacity>
+      </View>
+      {image && <Image source={{ uri: image }} className="mt-4 w-48 h-48" />}
+      {extractedText && (
+        <View className="flex flex-row items-center mt-4">
+          <Text className="text-sm">{extractedText}</Text>
+          <TouchableOpacity
+            onPress={copyToClipboard}
+            className="bg-gray-200 p-2 rounded-md"
+          >
+            <MaterialIcons name="content-copy" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
       )}
-      {extractedText && <Text>{extractedText}</Text>}
-      <Button title="Extract Content" onPress={extractContent} />
-    </View>
+      <TouchableOpacity
+        onPress={handleExtractText}
+        disabled={isExtracting}
+        className=" bg-red-500 rounded-full py-4 px-8 mt-6 shadow-lg"
+      >
+        <Text className="text-white font-bold text-xl">
+          {isExtracting ? "Extracting..." : "Extract Text"}
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
