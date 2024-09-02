4b6d52cb81e84bcee46e2bb5c42d6b01eaead3b8
bxljoy <259500313@qq.com>
2024-09-02 17:26:43 +0200

    feat: initial commit

---
+++ types/react-native.d.ts
@@ -0,0 +1,14 @@
+import { ViewProps, TextProps, ImageProps } from "react-native";
+
+declare module "react-native" {
+  interface ViewProps {
+    className?: string;
+  }
+  interface TextProps {
+    className?: string;
+  }
+  interface ImageProps {
+    className?: string;
+  }
+  // Add more components as needed.
+}
