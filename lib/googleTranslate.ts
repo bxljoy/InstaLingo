63f3bead5d4532e514398ef164baed045027d082
bxljoy <259500313@qq.com>
2024-09-05 02:37:07 +0200

    feat: update google translate api to use ApiKey for authentication

--- lib/googleTranslate.ts
+++ lib/googleTranslate.ts
@@ -1,15 +1,31 @@
+import * as SecureStore from "expo-secure-store";
+import { GOOGLE_TRANSLATE_API_KEY as INITIAL_API_KEY } from "../config";
+
 const GOOGLE_TRANSLATE_URL =
   "https://europe-central2-instalingo-434320.cloudfunctions.net/googleTranslate";
+const API_KEY_STORAGE_KEY = "google_translate_api_key";
+
+async function getApiKey(): Promise<string> {
+  let apiKey = await SecureStore.getItemAsync(API_KEY_STORAGE_KEY);
+  if (!apiKey) {
+    // If the API key isn't in secure storage, use the initial key and store it
+    apiKey = INITIAL_API_KEY;
+    await SecureStore.setItemAsync(API_KEY_STORAGE_KEY, apiKey);
+  }
+  return apiKey;
+}
 
 export async function translateText(
   text: string,
   targetLanguage: string
 ): Promise<string> {
   try {
+    const apiKey = await getApiKey();
     const response = await fetch(GOOGLE_TRANSLATE_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "X-API-Key": apiKey,
       },
       body: JSON.stringify({ text, targetLanguage }),
     });
