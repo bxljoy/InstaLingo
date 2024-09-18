91e052dc64daa136848ee204c032ffc742350123
bxljoy <259500313@qq.com>
2024-09-18 18:44:06 +0200

    feat: update AuthContext to handle user Authentication

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase/config";
+
+type AuthContextType = {
+  user: User | null;
+  isLoading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  isLoading: true,
+});
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setIsLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
