cc2f7cdeaa750ff4a6fb97d880e3ba5bc944efce
bxljoy <259500313@qq.com>
2024-09-28 14:35:55 +0200

    feat: increment version number

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default ({ config }) => ({
   ...config,
   name: "InstaLingo",
   slug: "instalingo",
-  version: "1.0.1",
+  version: "1.0.2",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "instalingo",
