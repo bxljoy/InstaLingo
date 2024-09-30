ad0a48ac236a1c0e2bc42d8a4710c0b6db2c26d7
bxljoy <259500313@qq.com>
2024-09-30 02:14:44 +0200

    feat: adding three prompt types of AI

--- app/(study)/learn.tsx
+++ app/(study)/learn.tsx
@@ -167,7 +167,7 @@ export default function LearnScreen() {
                 return (
                   <MaterialIcons
                     name="arrow-drop-down"
-                    size={24}
+                    size={48}
                     color="#007AFF"
                   />
                 );
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Image, TouchableOpacity, ScrollView } from "react-native";
+import {
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  View as RNView,
+} from "react-native";
 import { Text, View } from "@/components/Themed";
 import * as ImagePicker from "expo-image-picker";
 import { Camera } from "expo-camera";
@@ -10,12 +15,16 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
 import { apiWrapper } from "@/lib/apiWrapper";
 import ReviewPrompt from "@/components/ReviewPrompt";
+import RNPickerSelect from "react-native-picker-select";
 
 export default function HomeScreen() {
   const [hasPermission, setHasPermission] = useState(false);
   const [image, setImage] = useState<string | null>(null);
   const [isExtracting, setIsExtracting] = useState(false);
   const [isAnalyzing, setIsAnalyzing] = useState(false);
+  const [promptType, setPromptType] = useState<
+    "extract" | "identify" | "summary"
+  >("extract");
   const router = useRouter();
 
   useEffect(() => {
@@ -74,12 +83,18 @@ export default function HomeScreen() {
     setIsAnalyzing(true);
     try {
       if (image) {
-        const prompt =
-          "Analyze this image and Summarize the content of the image, then doing refining and context-aware adjustments";
-        // const prompt =
-        //   "Extract all text from this image using OCR and provide a summary of the main ideas. Highlight any important figures or names mentioned.";
-        // const prompt =
-        //   "Identify the product or objects in the image, and explain their potential use or value based on the image's content.";
+        let prompt = "";
+        if (promptType === "summary") {
+          prompt =
+            "Analyze this image and Summarize the content of the image, then doing refining and context-aware adjustments";
+        } else if (promptType === "identify") {
+          prompt =
+            "Identify the product or objects in the image, and explain their potential use or value based on the image's content.";
+        } else if (promptType === "extract") {
+          prompt =
+            "Extract all text from this image using OCR and provide a summary of the main ideas. Highlight any important figures or names mentioned.";
+        }
+
         const analysis = await apiWrapper(() => generateContent(prompt, image));
         router.push({
           pathname: "/analysis",
@@ -93,6 +108,12 @@ export default function HomeScreen() {
     }
   };
 
+  const promptOptions = [
+    { label: "Extract", value: "extract" },
+    { label: "Identify", value: "identify" },
+    { label: "Summary", value: "summary" },
+  ];
+
   if (hasPermission === null) {
     return <View />;
   }
@@ -102,12 +123,9 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <ScrollView className="flex-1 px-5 pt-2 pb-6">
-        <Text className="text-3xl font-bold text-black text-center mb-1">
-          InstaLingo
-        </Text>
-        <Text className="text-lg text-gray-600 text-center mb-4">
-          Convert your images to learning material
+      <ScrollView className="flex-1 px-5 pb-6">
+        <Text className="text-3xl font-extrabold text-gray-800 text-center mb-4">
+          Your Camera, Your Tutor
         </Text>
         <TouchableOpacity onPress={pickImage} activeOpacity={0.7}>
           {image ? (
@@ -122,27 +140,64 @@ export default function HomeScreen() {
             </View>
           )}
         </TouchableOpacity>
-        <View className="flex-row justify-around items-center w-full mt-2 rounded-xl bg-gray-200 p-3">
-          <TouchableOpacity onPress={takePicture} className="items-center">
-            <MaterialIcons name="camera-alt" size={40} color="#007AFF" />
-            <Text className="text-gray-600 mt-1 text-sm">Camera</Text>
+        <View className="flex-row justify-between items-center w-full mt-2 rounded-xl bg-gray-200 p-3">
+          <TouchableOpacity onPress={takePicture} className="items-center p-2">
+            <MaterialIcons name="camera-alt" size={48} color="#007AFF" />
           </TouchableOpacity>
           <TouchableOpacity
             onPress={handleExtractText}
             disabled={isExtracting || !image}
             className={`${
               image ? "bg-blue-500" : "bg-gray-300"
-            } rounded-full px-4 py-3`}
+            } rounded-full px-6 py-4`}
           >
             <Text className="text-white font-bold text-base text-center">
               {isExtracting ? "Extracting..." : "Extract Text"}
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={pickImage} className="items-center">
-            <MaterialIcons name="photo" size={40} color="#007AFF" />
-            <Text className="text-gray-600 mt-1 text-sm">Album</Text>
+          <TouchableOpacity onPress={pickImage} className="items-center p-2">
+            <MaterialIcons name="photo" size={48} color="#007AFF" />
           </TouchableOpacity>
         </View>
+        <RNView className="mt-4 mb-4">
+          <Text className="text-gray-700 mb-2">Select Analysis Type:</Text>
+          <RNPickerSelect
+            onValueChange={(value) => setPromptType(value)}
+            items={promptOptions}
+            value={promptType}
+            style={{
+              inputIOS: {
+                fontSize: 16,
+                paddingVertical: 12,
+                paddingHorizontal: 10,
+                borderWidth: 1,
+                borderColor: "gray",
+                borderRadius: 4,
+                color: "black",
+                paddingRight: 30,
+              },
+              inputAndroid: {
+                fontSize: 16,
+                paddingHorizontal: 10,
+                paddingVertical: 8,
+                borderWidth: 1,
+                borderColor: "gray",
+                borderRadius: 8,
+                color: "black",
+                paddingRight: 30,
+              },
+            }}
+            Icon={() => {
+              return (
+                <MaterialIcons
+                  name="arrow-drop-down"
+                  size={48}
+                  color="#007AFF"
+                />
+              );
+            }}
+          />
+        </RNView>
         <TouchableOpacity
           onPress={handleAnalyze}
           disabled={isAnalyzing || !image}
