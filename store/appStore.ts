1bad50ee99dd4f26211f53b501be13c4d02ab8d0
bxljoy <259500313@qq.com>
2024-10-06 11:52:46 +0200

    feat: seperated the Selectors from store

--- store/appStore.ts
+++ store/appStore.ts
@@ -1,21 +1,6 @@
 import { User } from "firebase/auth";
-import { StoreApi, UseBoundStore, create } from "zustand";
-
-type WithSelectors<S> = S extends { getState: () => infer T }
-  ? S & { use: { [K in keyof T]: () => T[K] } }
-  : never;
-
-const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
-  _store: S
-) => {
-  let store = _store as WithSelectors<typeof _store>;
-  store.use = {};
-  for (let k of Object.keys(store.getState())) {
-    (store.use as any)[k] = () => store((s) => s[k as keyof typeof s]);
-  }
-
-  return store;
-};
+import { create } from "zustand";
+import { createSelectors } from "./createSelectors";
 
 interface State {
   user: User | null;
